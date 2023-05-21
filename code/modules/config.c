6f93d15adfdb2ae63241f93d9036305e1f0bbcd0
Simone <simonescaccia.s@gmail.com>
2023-05-22 01:50:55 +0200

    fix structure

--- code/modules/config.c
+++ code/modules/config.c
@@ -2,8 +2,8 @@ char* config(void) {
 
     /* String format: space-separated <x>-<y> pairs, with x for father and y for child */
     char* buffer = "10-11 "
-                   "12-13 "
-                   "12-14";
+                   "11-12 "
+                   "11-13";
     
     return buffer;
 }
