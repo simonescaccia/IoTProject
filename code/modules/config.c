0da585688960f6d801c615e82ac617fad0d10c29
Simone <simonescaccia.s@gmail.com>
2023-05-24 12:43:54 +0200

    Changing config

--- code/modules/config.c
+++ code/modules/config.c
@@ -1,9 +1,9 @@
 char* config(void) {
 
     /* String format: space-separated <x>-<y> pairs, with x for father and y for child */
-    char* buffer = "10-11 "
-                   "11-12 "
-                   "11-13";
+    char* buffer = "11-12 "
+                   "12-13 "
+                   "12-14";
     
     return buffer;
 }
