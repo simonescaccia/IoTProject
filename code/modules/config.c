df5e5c0e0708d9b6d658bd56c62d4cf5d0191e7e
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-05-25 16:18:16 +0200

    Update config.c

--- code/modules/config.c
+++ code/modules/config.c
@@ -1,9 +1,12 @@
 char* config(void) {
 
-    /* String format: space-separated <x>-<y> pairs, with x for father and y for child */
+    /* String format (space-separated pairs):
+    first pair is <source_ttn>-<source_p2p>, then
+    <x>-<y> pairs, with x for father and y for child 
+    */
     char* buffer = "11-12 "
                    "12-13 "
-                   "12-14";
+                   "13-14";
     
     return buffer;
 }
