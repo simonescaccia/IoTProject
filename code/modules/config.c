20e114bd1b386f0acc69c48a74b2e4c8eab8073a
Simone <simonescaccia.s@gmail.com>
2023-05-29 09:26:37 +0200

    fix config

--- code/modules/config.c
+++ code/modules/config.c
@@ -4,9 +4,10 @@ char* config(void) {
     first pair is <source_ttn>-<source_p2p>, then
     <x>-<y> pairs, with x for father and y for child 
     */
-    char* buffer = "1-17 "
-                   "17-18 "
-                   "18-20";
+    char* buffer = "1-10 "
+                   "10-11 "
+                   "11-18 "
+                   "11-19";
     
     return buffer;
 }
