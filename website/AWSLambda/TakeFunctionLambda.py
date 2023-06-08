6f6a7d8d9c2aaebc20991f33897268fa3943c12f
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2023-06-08 17:59:44 +0200

    Update TakeFunctionLambda.py

--- website/AWSLambda/TakeFunctionLambda.py
+++ website/AWSLambda/TakeFunctionLambda.py
@@ -1,4 +1,3 @@
- 
 import json
 
 # import the AWS SDK
@@ -14,33 +13,28 @@ table_leakage = dynamodb.Table('WaterLeakageTable')
 # Define the handler function that the Lambda service will use
 def lambda_handler(event, context):
     message = event['uplink_message']
-    print(message)
-    print()
-    payload = message['decoded_payload']
-    print(payload)
-    print()
-    test = payload["myTestValue"]
-    print(test)
-    print()
-    print(test['Id'])
 
+    payload = message['decoded_payload']
+
+    test = payload["myTestValue"]
+
+    
     if (test['Id']=='flow') :
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_flow.put_item(
             Item={
                 'Datetime': test['Datetime'],
-                'Flow': str(event['Flow'])
+                'Flow': test['Flow']
                 })
-        response = table_flow.scan()
-        output = response['Items']
-
-        print(response['Items'])
-
+        #response = table_flow.scan()
+        #output = response['Items']
+    
+        #print(response['Items'])
+        
         return {
-           'statusCode': 200,
-           'body': json.dumps(output)
-        }
-
+           'statusCode': 200
+        }        
+                
     else:
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_leakage.put_item(
@@ -50,14 +44,12 @@ def lambda_handler(event, context):
                 'Father': test["Father"],
                 'Leakage': test["Leakage"]
                 })
-
-        response = table_leakage.scan()
-        output = response['Items']
-
-        print(response['Items'])
-
+            
+        #response = table_leakage.scan()
+        #output = response['Items']
+    
+        #print(response['Items'])
+        
         return {
-           'statusCode': 200,
-           'body': json.dumps(output)
-
+           'statusCode': 200
         }
