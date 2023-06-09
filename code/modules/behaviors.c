6c776b76d1bc5e663bec752e1edf008935cae65c
Pasquale Mocerino <100220814+pasqualemocerino@users.noreply.github.com>
2023-06-09 18:43:24 +0200

    Update behaviors.c

--- code/modules/behaviors.c
+++ code/modules/behaviors.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "xtimer.h"
 #include "mutex.h"
@@ -41,9 +42,8 @@ int source_lora_ttn(node_t node)
     /* Sampling time */
     int s_time = -1;
     /* Current date and time */
-    //char datetime[20];
-    //time_t current;
-
+    char datetime[20];
+    time_t t;
      /* Set TTN application parameters */
     char* deveui_list[4] = {"loramac", "set", "deveui", DEV_EUI};
     char** argv = (char**)&deveui_list;
@@ -66,28 +66,14 @@ int source_lora_ttn(node_t node)
     loramac_handler(3,argv);
 
     while(1) {
-        /*time(&current);
-        struct tm* t = localtime(&current);
-        int c = strftime(datetime, sizeof(datetime), "%Y-%m-%d %T", t);
-        if(c == 0) {
-            printf("Error: invalid format.\n");
-            return -1;
-        }*/
-        
-        /* Set time for sampling: [0, inf) */
-        s_time = (s_time + 1);
+        /* Set time for sampling: [0, 60] */
+        s_time = (s_time+1) % 10;
 
         /* Get water flow value */
         int water_flow = get_water_flow(node.node_type, 0, s_time);
-        /* Fill json document */
-        sprintf(json, "{\"node_id\": \"%s\", \"datetime\": \"%s\", \"water_flow\": \"%d\"}", node.node_self, message, water_flow);
-        
+        /* Send source water flow information */
+        sprintf(json, "{\"Id\": \"flow\", \"Flow\": \"%d\"}", water_flow);
         puts(json);
-
-        /* Send a message 
-        char* send_list[2] = {"send_cmd", json};
-        argv = (char**)&send_list;
-        send_cmd(2, argv);*/
 
         char* tx_list[3] = {"loramac", "tx", json};
         argv = (char**)&tx_list;
@@ -95,6 +81,89 @@ int source_lora_ttn(node_t node)
 
         /* Sleeping for five seconds */
         xtimer_sleep(5);
+
+        /* ------------------------------------------------------------------------------- */
+
+        /* Number of nodes */
+        int n = 4;
+        char **nodes = malloc(n * sizeof(char *));
+
+        /* Starting node number */
+        int  num = 11;
+
+        char number[5];  
+        char* child, *father;
+        int leakage;
+        float threshold = 1.0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+            nodes[i] = malloc(15 * sizeof(char));
+
+            strcpy(nodes[i], "st-lrwan1-"); 
+            sprintf(number,"%d", num);
+            strcat(nodes[i], number);
+
+            num++;
+
+            }
+            
+            if (i < n - 1) {
+                nodes[i + 1] = malloc(15 * sizeof(char));
+
+                strcpy(nodes[i + 1], "st-lrwan1-"); 
+                sprintf(number,"%d", num);
+                strcat(nodes[i + 1], number);
+
+                num++;
+
+                father = malloc(15 * sizeof(char));
+                strcpy(father, nodes[i]);
+                child = malloc(15 * sizeof(char));
+                strcpy(child, nodes[i + 1]);
+
+                float father_water_flow, child_water_flow;
+                
+
+                if (rand() % 2 == 0) {
+                    /* CHIEF - FORK */
+                    father_water_flow = get_water_flow(1, 0, s_time); // 2;
+                    child_water_flow = get_water_flow(2, 0, s_time); //1.5;
+                }
+
+                else {
+                    /* FORK - BRANCH */
+                    father_water_flow = get_water_flow(2, 0, s_time); // 5;
+                    child_water_flow = get_water_flow(3, 0, s_time); // 3;
+                } 
+
+                if (fabs(father_water_flow - child_water_flow) > threshold) {
+
+                    leakage = fabs(father_water_flow - child_water_flow);
+
+                    /* Send leakage information*/
+                    sprintf(json, "{\"Id\": \"leakage\", \"Child\": \"%s\", \"Father\": \"%s\", \"Leakage\": \"%f\"}", child, father, leakage); 
+                    puts(json); 
+
+                    char* lx_list[3] = {"loramac", "tx", json};
+                    argv = (char**)&lx_list;
+                    loramac_handler(3, argv);
+
+                    /* Sleeping for five seconds */
+                    xtimer_sleep(5);    
+
+                }
+            }
+        }
+        
+        /* ------------------------------------------- */
+        
+        /* sprintf(json, "{\"Id\": \"leakage\", \"Child\": \"%s\", \"Father\": \"%s\", \"Leakage\": \"%f\"}", "st-lrwan1-11", "st-lrwan1-12", 10); 
+        puts(json);
+        char* lx_list[3] = {"loramac", "tx", json};
+        argv = (char**)&lx_list;
+        loramac_handler(3, argv);
+        xtimer_sleep(5); */
 
         /* AWS integration */
         /* TTNClient = mqtt.Client()
