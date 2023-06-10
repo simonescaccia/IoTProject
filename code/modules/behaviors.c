2659f501e9e2fcf7188ff1c6717a0bc0fecf7048
Simone <simonescaccia.s@gmail.com>
2023-06-10 18:23:24 +0200

    fix

--- code/modules/behaviors.c
+++ code/modules/behaviors.c
@@ -363,7 +363,7 @@ int lora_p2p(node_t node) {
     xtimer_ticks32_t last_wakeup;
     bool is_last_wakeup = false;
     /* Starting logic time for the sample generator */
-    int time = 5;
+    int time = -1;
 
     _start_listening();
 
