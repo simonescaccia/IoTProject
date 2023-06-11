5e7641bc6248c9648948dfb7d1dc0ee858b827d6
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2023-06-11 17:28:43 +0200

    Update TakeFunctionLambda.py

--- website/AWSLambda/TakeFunctionLambda.py
+++ website/AWSLambda/TakeFunctionLambda.py
@@ -3,12 +3,16 @@ import json
 # import the AWS SDK
 import boto3
 
+from datetime import datetime
+
 # Create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 
 # Use the DynamoDB object to select our table
 table_flow = dynamodb.Table('WaterFlowSourceTable')
 table_leakage = dynamodb.Table('WaterLeakageTable')
+
+time_now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
 # Define the handler function that the Lambda service will use
 def lambda_handler(event, context):
@@ -19,17 +23,18 @@ def lambda_handler(event, context):
     test = payload["myTestValue"]
 
     
+    
     if (test['Id']=='flow') :
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_flow.put_item(
             Item={
-                'Datetime': test['Datetime'],
+                'Datetime': time_now,
                 'Flow': test['Flow']
                 })
         #response = table_flow.scan()
         #output = response['Items']
     
-        #print(response['Items'])
+        print(response['Items'])
         
         return {
            'statusCode': 200
@@ -39,7 +44,7 @@ def lambda_handler(event, context):
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_leakage.put_item(
             Item={
-                'Datetime': test["Datetime"],
+                'Datetime': time_now,
                 'Child': test["Child"],
                 'Father': test["Father"],
                 'Leakage': test["Leakage"]
@@ -48,7 +53,7 @@ def lambda_handler(event, context):
         #response = table_leakage.scan()
         #output = response['Items']
     
-        #print(response['Items'])
+        print(response['Items'])
         
         return {
            'statusCode': 200
