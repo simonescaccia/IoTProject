8ccd6baed6cfe9b34432ad6c4be71e2563d3e1eb
Simone <simonescaccia.s@gmail.com>
2023-06-12 15:10:14 +0200

    fix sending period

--- code/modules/params.c
+++ code/modules/params.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
+#include <params.h>
 
 uint32_t const SIMULATED_DAY = 30;
 
 uint32_t const SAMPLING_PLUS_DATA_PROCESSING = 2;
 
+#if DUTY_CYCLE == 1 
 uint32_t const NUMBER_OF_SENDING_PER_DAY = 3;
+#else
+uint32_t const NUMBER_OF_SENDING_PER_DAY = 1;
+#endif
+
 uint32_t const LEAKAGE_TEST_PERIOD = SIMULATED_DAY/NUMBER_OF_SENDING_PER_DAY;
 
 uint32_t const LISTENING_TIMEOUT = 2*SAMPLING_PLUS_DATA_PROCESSING+LEAKAGE_TEST_PERIOD;
