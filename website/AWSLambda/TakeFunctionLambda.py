7d16cf92842f3a26a3a30b7ff1b9781d596eee24
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2023-06-18 15:11:29 +0200

    Update TakeFunctionLambda.py

--- website/AWSLambda/TakeFunctionLambda.py
+++ website/AWSLambda/TakeFunctionLambda.py
@@ -1,9 +1,9 @@
 import json
-
 # import the AWS SDK
 import boto3
-
+import calendar
 from datetime import datetime
+import time
 
 # Create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
@@ -12,11 +12,16 @@ dynamodb = boto3.resource('dynamodb')
 table_flow = dynamodb.Table('WaterFlowSourceTable')
 table_leakage = dynamodb.Table('WaterLeakageTable')
 
-# Generate a timestamp for the event
-time_now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
 # Define the handler function that the Lambda service will use
 def lambda_handler(event, context):
+    
+    # Generate a timestamp for the event
+    time_now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+    current_GMT = time.gmtime()
+    timestamp = calendar.timegm(current_GMT)
+    
     message = event['uplink_message']
 
     payload = message['decoded_payload']
@@ -28,25 +33,23 @@ def lambda_handler(event, context):
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_flow.put_item(
             Item={
+                'Timestamp': timestamp,
                 'Datetime': time_now,
                 'Flow': test['Flow']
                 })
         
-        return {
-           'statusCode': 200
-        }        
                 
     else:
         # Write payload and time to the DynamoDB table using the object we instantiated and save response in a variable
         response = table_leakage.put_item(
             Item={
-                'Datetime': time_now,
+                'Timestamp': timestamp,
+                'Datetime': time_now, 
                 'Child': test["Child"],
                 'Father': test["Father"],
                 'Leakage': test["Leakage"]
                 })
             
-        
-        return {
-           'statusCode': 200
-        }
+    return {
+       'statusCode': 200
+    }
